f6f6d4bd1489a656e8a12460fc9f2cd620449f98
zhifu.chen <zhifu.chen@fu-fang.com>
2017-04-27 14:02:21 +0800

    提交1

---
+++ src/main/java/com/fufang/cloud/Test1.java
@@ -0,0 +1,10 @@
+package com.fufang.cloud;
+
+/**
+ * Created by zhifu.chen on 2017/4/27.
+ */
+public class Test1 {
+    public static void main(String[] args) {
+        System.out.println(33);
+    }
+}
