263f08e71512214b369250cae1cefe070622cb08
zhifu.chen <zhifu.chen@fu-fang.com>
2017-04-27 14:02:42 +0800

    提交2

---
+++ src/main/java/com/fufang/cloud/Test2.java
@@ -0,0 +1,10 @@
+package com.fufang.cloud;
+
+/**
+ * Created by zhifu.chen on 2017/4/27.
+ */
+public class Test2 {
+    public static void main(String[] args) {
+        System.out.println(2);
+    }
+}
