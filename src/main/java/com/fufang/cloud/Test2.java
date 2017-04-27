4629bf978fd832a1bd7441bb4934d350771263fd
zhifu.chen <zhifu.chen@fu-fang.com>
2017-04-27 16:07:46 +0800

    修改2

--- src/main/java/com/fufang/cloud/Test2.java
+++ src/main/java/com/fufang/cloud/Test2.java
@@ -4,6 +4,9 @@ package com.fufang.cloud;
  * Created by zhifu.chen on 2017/4/27.
  */
 public class Test2 {
+    public static void main(String[] args) {
+        System.out.println("ddddddddddddddddddddddddd");
+    }
     public static void main2(String[] args) {
         System.out.println(2);
     }
