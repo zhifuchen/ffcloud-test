233cb6509157933278e2550510b7b152029f62bc
zhifu.chen <zhifu.chen@fu-fang.com>
2017-06-19 21:22:10 +0800

    提交

--- src/main/java/com/fufang/cloud/service/impl/WithdrawApplyServiceImpl.java
+++ src/main/java/com/fufang/cloud/service/impl/WithdrawApplyServiceImpl.java
@@ -38,6 +38,5 @@ public class WithdrawApplyServiceImpl implements IWithdrawApplyService {
         account.setPharmacyId(2);
         account.setCreateDate(createTime);
         accountMapper.insert(account);
-        throw new RuntimeException();
     }
 }
